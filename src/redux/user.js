bbe07ce906f901c40cb211541b398e88b57b5d8c
Luciano <luciano.miguel.ch95@gmail.com>
2023-03-27 16:28:13 -0300

    user reducer bugfix

--- src/redux/user.js
+++ src/redux/user.js
@@ -3,12 +3,7 @@ import { createAction, createReducer } from "@reduxjs/toolkit";
 export const logIn = createAction("LOG_IN");
 export const logOut = createAction("LOG_OUT");
 
-const user =
-  localStorage.getItem("profile") !== null
-    ? JSON.parse(localStorage.getItem("profile"))
-    : null;
-
-const initialState = user;
+const initialState = null;
 
 const userReducer = createReducer(initialState, {
   [logIn]: (state, action) => (state = action.payload),

a832fb5c629d3cbc4b999a8af23816a4b46edd04
sergiogenes <genessergio@gmail.com>
2023-03-28 18:51:49 -0300

    added user persistence in Redux user state

--- src/redux/user.js
+++ src/redux/user.js
@@ -1,9 +1,14 @@
 import { createAction, createReducer } from "@reduxjs/toolkit";
+import Cookies from "js-cookie";
+import jwt_decode from "jwt-decode";
 
 export const logIn = createAction("LOG_IN");
 export const logOut = createAction("LOG_OUT");
 
-const initialState = null;
+const token = Cookies.get("token");
+const user = token ? jwt_decode(token) : {};
+
+const initialState = user;
 
 const userReducer = createReducer(initialState, {
   [logIn]: (state, action) => (state = action.payload),

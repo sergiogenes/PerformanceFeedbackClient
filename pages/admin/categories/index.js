956ddb501ea43e2117920ea53f573097675cf159
alanff97 <alan.figueroa.cl@gmail.com>
2023-04-05 14:58:57 +0200

    category page

--- pages/admin/categories/index.js
+++ pages/admin/categories/index.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { Navbar } from "../../../components/Navbar";
+import CategoryTable from "../../../commons/AdminTables/CategoryTable";
 const categories = () => {
-  return <div>Categorias</div>;
+  return <Navbar Component={CategoryTable} />;
 };
 
 export default categories;

16a4b62471eba1ac5f8a93ee107a6543200db55e
sergiogenes <genessergio@gmail.com>
2023-05-18 13:47:07 -0300

    modificaciones en el roteo

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: "https://performance-feedback-server.vercel.app/:path*",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
